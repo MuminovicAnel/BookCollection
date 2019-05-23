ffbca3f9e3a1ff9ec6d038a3d5d730022388dd89
Muminovic Anel <muminovic____@hotmail.ch>
2019-05-23 10:28:48 +0200

    create details page for books

---
+++ src/app/books-details/books-details.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { BooksDetailsPage } from './books-details.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BooksDetailsPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [BooksDetailsPage]
+})
+export class BooksDetailsPageModule {}
