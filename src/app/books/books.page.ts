3ee9eb19fa5b1995b3d0ffb39d182cc96ea46f64
Muminovic Anel <Muminovic____@hotmail.ch>
2019-05-28 16:37:08 +0200

    Add debounce time when searching

--- src/app/books/books.page.ts
+++ src/app/books/books.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BooksService, SearchType } from '../api/books.service';
+import { LoadingController, IonSearchbar } from '@ionic/angular';
+import { ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-books',
@@ -8,12 +11,13 @@ import { BooksService, SearchType } from '../api/books.service';
   styleUrls: ['./books.page.scss']
 })
 export class BooksPage implements OnInit {
+  @ViewChild('mainSearchbar') searchBar: IonSearchbar;
 
   results: Observable<any>;
   searchTerm: string = '';
   type: SearchType = SearchType.all;
 
-  constructor(private booksService: BooksService) { }
+  constructor(private booksService: BooksService, private loadingController: LoadingController) { }
 
   ngOnInit() {
   }
@@ -21,6 +25,21 @@ export class BooksPage implements OnInit {
   searchChanged() {    
     // Call our service function which returns an Observable
     this.results = this.booksService.searchData(this.searchTerm, this.type);
+  }
+
+  async presentLoadingWithOptions() {
+    const loading = await this.loadingController.create({
+      spinner: null,
+      duration: 400,
+      message: 'Please wait...',
+      translucent: true,
+      cssClass: 'custom-class custom-loading'
+    });
+    return await loading.present();
+  }
+
+  setFocus(){
+    this.searchBar.setFocus();
   }
 
 }
