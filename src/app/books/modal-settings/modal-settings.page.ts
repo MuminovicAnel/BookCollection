7c5ba52a306e561c2bff4e0142304d661a71c283
muminovicanel <muminovic____@hotmail.ch>
2019-06-12 23:47:45 +0200

    Add language setting

--- src/app/books/modal-settings/modal-settings.page.ts
+++ src/app/books/modal-settings/modal-settings.page.ts
@@ -25,7 +25,7 @@ export class ModalSettingsPage implements OnInit {
   ngOnInit() {
     console.table(this.navParams);
     this.languages = this.navParams.data.lang;
-    console.log(this.languages)
+    console.log(this.lang)
     this.storage.get(STORAGE_KEY).then((value) => {
       this.storedLang = value;
     });
@@ -36,8 +36,11 @@ export class ModalSettingsPage implements OnInit {
   }
 
   async onDismiss() {
+    console.log(this.lang);
     this.storage.set(STORAGE_KEY, this.lang)
   }
+  
+  
 
 
 }
