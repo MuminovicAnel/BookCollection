aaa0612e9f3c5052b79326d33c1381c3fe675f13
Muminovic Anel <Muminovic____@hotmail.ch>
2019-06-14 14:39:46 +0200

    Storage set for lang and max result

--- src/app/books/modal-settings/modal-settings.page.ts
+++ src/app/books/modal-settings/modal-settings.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams, PickerController } from '@ionic/angular';
 import { langRestrict } from '../../api/books.service';
 import { Storage } from '@ionic/storage';
-import { PickerColumnOption, PickerOptions } from '@ionic/core';
+import { PickerOptions } from '@ionic/core';
 import { BooksService, } from '../../api/books.service';
 
 const STORAGE_KEY = 'settings';
@@ -17,8 +17,9 @@ export class ModalSettingsPage implements OnInit {
   private languages: Array<string>;
   private lang = [] = langRestrict;
   private storedLang: string;
-  private numbers: PickerColumnOption[];
-  private results = "";
+  private number: string;
+  private textNumber = '';
+  private maxResult: any;
  
   constructor(
     private modalController: ModalController,
@@ -26,49 +27,51 @@ export class ModalSettingsPage implements OnInit {
     private storage: Storage,
     private pickerCtrl: PickerController,
     private booksService: BooksService
-  ) {
-    this.numbers = Array.from(new Array(31), (x,i) => i+10);
-   }
+  ) { }
  
   ngOnInit() {
     console.table(this.navParams);
     this.languages = this.navParams.data.lang;
-    console.log(this.lang)
     this.storage.get(STORAGE_KEY).then(value => {
-      this.storedLang = value['key'];
+      value.forEach(item => {
+        console.log(item)
+        if(item) {
+          this.storedLang = item['key'];
+          this.number = item;
+        }
+      });
     });
-    console.log(this.numbers)
   }
  
   async closeModal() {
     await this.modalController.dismiss()
-    console.log(this.lang);
     this.languages.forEach(item => {
       if(item['value'] === this.lang) {
         this.lang = item;
-        console.log(this.lang)
       }
     });
-    console.log(this.lang)
-    this.booksService.getAllFavoriteBooks(STORAGE_KEY).then(result => {
-        console.log(this.lang['value'])
-        console.log(result.value)
-        if(result.value === this.lang['value']) {
-          //this.booksService.favoriteBook(this.lang, STORAGE_KEY)
-        } else {
-          let res = result
-          console.log(res.value)
+      // Set the language setting and overwrite previous value
+      this.booksService.getAllFavoriteBooks(STORAGE_KEY).then(result => {
+        result.forEach(item => {
+          if(item.value === this.lang['value']) {
+            //this.booksService.favoriteBook(this.lang, STORAGE_KEY)
+          } else {
+            let res = [] = item
             res.value = this.lang['value']
             res.key = this.lang['key']
-          this.storage.set(STORAGE_KEY, res);
-        }
-    });  
+            this.storage.set(STORAGE_KEY, [res])
+          }
+      });
+    });
+    if(this.maxResult) {
+      this.booksService.favoriteBook(this.maxResult, STORAGE_KEY)
+    }
+
   }
 
   // Picker 
   async pickerComp() {
     let options = [];
-    console.log(options)
     for (let i = 10; i <= 40; i++) {
       options.push({
         text: i.toString(), value: i
@@ -97,12 +100,11 @@ export class ModalSettingsPage implements OnInit {
     let picker = await this.pickerCtrl.create(opts);
 
     picker.onDidDismiss().then(async data => {
-      console.log(data)
-      this.storage.set(STORAGE_KEY, this.numbers)
       let col = await picker.getColumn('MaxResult');
-      console.log(col)
-      this.results = col.options[col.selectedIndex].text;
-      console.log(this.results)
+      this.textNumber = col.options[col.selectedIndex].text;
+      console.log(this.textNumber)
+      this.maxResult = col.options[col.selectedIndex];
+      console.log(this.maxResult)
     });
 
     return await picker.present();
